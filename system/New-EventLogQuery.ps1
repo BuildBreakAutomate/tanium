6cfe15ce61b063679041be2f8b498425b7cae4e6
BuildBreakAutomate <60624976+BuildBreakAutomate@users.noreply.github.com>
2026-01-15 10:45:36 -0500

    Update New-EventLogQuery.ps1
    Forgot the disclaimer / contact stuff.

--- system/New-EventLogQuery.ps1
+++ system/New-EventLogQuery.ps1
@@ -43,6 +43,33 @@
     Output format: TimeCreated~EventID~OpCode~Message~Account~SID
     Returns "No matching events detected" if no events found.
     Returns error details if query fails.
+.AUTHOR
+    Brent M. Henderson - Build Break Automate LLC
+	Copyright (c) 2025 - Build Break Automate LLC. - https://buildbreakautomate.com
+	Need help with implementation? Contact me at https://buildbreakautomate.com/index.php/need-help/ for implementation / consulting services.
+	Always happy to meet new people; add me on LinkedIn at https://www.linkedin.com/in/brentmhenderson/.
+.LICENSE
+    MIT License
+
+    Copyright (c) 2026 Build Break Automate LLC
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 #>
 
 # Tanium parameter ingestion with defaults
@@ -200,4 +227,5 @@ foreach ($Event in $Events) {
     $sidValue = if ($sid.SID) { $sid.SID } else { 'N/A' }
     
     "$($Event.TimeCreated.ToString('yyyy-MM-dd HH:mm:ss'))~$($Event.Id)~$($Event.OpcodeDisplayName)~$cleanMessage~$accountName~$sidValue"
+
 }
